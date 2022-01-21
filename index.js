b8f95d5e51b776eb2cd8c7aac24d0a31c407f227
itsjustrobert <robertlove99999@gmail.com>
2022-01-21 12:37:06 -0600

    fixed the navbar

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const populate = () => {
+  let projectList = document.getElementById('project-list');
     let projectImages = ["dice", "api", "youtube", "bank"];
+    let projectNames = ['Farkle','Proxy Web Server', 'Youtube Clone','Bank App']
     let links = ['https://github.com/cs-memphis-c-py-401d1cd/lab06-greed-project-demarcus_robert_spencer','https://github.com/cs-memphis-code-301d3/code301-lab8-react-node-express-robertCodedIt','https://github.com/cs-memphis-code-301d3/301-group-project-final-lizzy-jerrod-robert','https://github.com/cs-memphis-code-301d3/mern-assessment-1-robertCodedIt']
-    let descriptions=['dice project using python','api project leveraging nodejs','youtube project using nodejs','bank project using react']
+    let descriptions=['Farkle Console Game using Python','Proxy Web Server leveraging NodeJS','Youtube clone using the MERN stack ','Banking App written in ReactJS']
     let projects = document.getElementById("projects");
+    // for(let i =0;i<projectImages.length;i++){
+      
+     
+    // }
     for (let i = 0; i < projectImages.length; i++) {
       let images = document.createElement("img");
       images.src = `./assets/images/${projectImages[i]}.jpg`;
-      images.alt = `${projectImages[i]}`;
+      images.alt = `${projectNames[i]}`;
   
       let a = document.createElement("a");
       a.appendChild(images);
@@ -14,14 +20,18 @@ const populate = () => {
       // a.textContent = `Project Name: ${projectImages[i]} `
       // a.id = `${projectImages[i]}`;
       a.href = `${links[i]}`;
+      a.id = `${projectNames[i]}`
       a.target = 'blank'
       let b = document.createElement("div").appendChild(a);
       b.classList = 'blur';
       b.style.display = 'grid'
       b.style.placeItems = 'center'
       b.style.margin = '.4rem'
-     
-     
+      // create project names and links in the dropdown menu
+      let projectATags = document.createElement('a');
+      projectATags.innerText = `${projectNames[i]}`
+      projectATags.href = '#' + projectNames[i];
+      projectList.appendChild(projectATags);
       c = document.createElement('p');
       c.innerText = descriptions[i];
       b.appendChild(c)
